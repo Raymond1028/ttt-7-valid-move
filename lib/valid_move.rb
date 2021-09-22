824e77d0614ecc8d966fe0e569242f9ad0781266
Ray's Git <lancelot53.ra@gmail.com>
2021-09-22 16:54:52 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,12 @@
-# code your #valid_move? method here
+def  valid_move?(board, int)
+   if board[int] == " " || board[int] == ""
+    return true
+   #elsif int < 0 || int > 9
+    #return false
+   end
+    
+end
+    # code your #valid_move? method here
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
